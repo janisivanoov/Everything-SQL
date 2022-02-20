76582a8c0e6ddd80e11a59904cb847fc5d186141
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-02-20 12:20:11 +0200

    Add files via upload

---
+++ task_1_practice.sql
@@ -0,0 +1,6 @@
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`, `emp_firstname`, `emp_lastname`) VALUES ('1', 'John', 'Sinderello');
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`, `emp_firstname`, `emp_lastname`) VALUES ('2', 'Peter', 'Antonio');
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`, `emp_firstname`, `emp_lastname`) VALUES ('3', 'Isaack', 'Neweson');
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`, `emp_firstname`, `emp_lastname`) VALUES ('4', 'Joshua', 'Nellson');
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`, `emp_firstname`, `emp_lastname`) VALUES ('5', 'Helen', 'Anastasiou');
+INSERT INTO `task1`.`test_table_employees_1` (`emp_id`,`emp_firstname`, `emp_lastname`) VALUES ('6','Andrew', 'Fillatov');
